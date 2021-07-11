bf4d6a3f1c368bc8cf361e834d07d9de4592d383
Zuzanna <zsawala@prograils.com>
2021-07-11 23:53:20 +0200

    Cleanup
    Remove useless factories

--- spec/factories/reviews.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :review do
-    scope { 1 }
-    movie { nil }
-  end
-end
--- spec/factories/user_movies.rb
+++
@@ -1,6 +0,0 @@
-FactoryBot.define do
-  factory :user_movie do
-    user { nil }
-    movie { nil }
-  end
-end
